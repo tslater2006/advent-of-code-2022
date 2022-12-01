2645e3e85e65d0998b1268e9b9303ced2053a51b
Tim Slater <tim.slater@intrasee.com>
2022-11-30 23:10:23 -0600

    Initial Day 1 Solution

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,54 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] values;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        values = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() {
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        List<int> elfCalories = new List<int>();
+        int currentSum = 0;
+        foreach (var s in values)
+        {
+            if (s == "")
+            {
+                elfCalories.Add(currentSum);    
+                currentSum = 0;
+            } else
+            {
+                currentSum += int.Parse(s);
+            }
+        }
+
+        elfCalories.Add(currentSum);
+        return new(elfCalories.Max().ToString());
+    }
+    public override ValueTask<string> Solve_2()
+    {
+        List<int> elfCalories = new List<int>();
+        int currentSum = 0;
+        foreach (var s in values)
+        {
+            if (s == "")
+            {
+                elfCalories.Add(currentSum);
+                currentSum = 0;
+            }
+            else
+            {
+                currentSum += int.Parse(s);
+            }
+        }
+
+        elfCalories.Add(currentSum);
+
+        int answer = elfCalories.OrderByDescending(x => x).Take(3).Sum();
+
+        return new(answer.ToString());
+
+    }
 }
