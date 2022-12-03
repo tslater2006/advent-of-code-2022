04cb6f4560e9cf4c7bbdc61cf2228f7a17bf8c43
Tim Slater <tim.slater@intrasee.com>
2022-12-03 08:30:56 -0600

    Change Day 3 to LINQ one liners

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -10,79 +10,14 @@ public class Day03 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        List<char> foundDuplicates = new();
-        foreach(var ruck in rucksacks)
-        {
-            List<char> firstHalfChars = new();
-
-            for(var x = 0; x < ruck.Length; x++)
-            {
-                if (x < ruck.Length/2)
-                {
-                    firstHalfChars.Add(ruck[x]);
-                } else
-                {
-                    if (firstHalfChars.Contains(ruck[x]))
-                    {
-                        /* found the dup */
-                        foundDuplicates.Add(ruck[x]);
-                        break;
-                    }
-                }
-            }
-        }
-
-        var answer = foundDuplicates.Sum(x =>
-        {
-            if (x >= 97)
-            {
-                return x - 96;
-            } else
-            {
-                return x - 64 + 26;
-            }
-        });
-
-
-        return new(answer.ToString());
+        var ans = rucksacks.Select(ruck => ruck[0..(ruck.Length / 2)].Intersect(ruck[(ruck.Length / 2)..]).Distinct().First()).Sum(r => r >= 97 ? r - 96 : r - 64 + 26);
+        return new(ans.ToString());
 
     }
     public override ValueTask<string> Solve_2()
     {
-        List<char> badges = new();
-        foreach (var elfs in rucksacks.Chunk(3))
-        {
-            List<char> workingSet = new(elfs[0].ToCharArray());
-            List<char> commonChars = new();
-            foreach (var elf in elfs.Skip(1))
-            {
-                foreach( var c in elf)
-                {
-                    if (workingSet.Contains(c))
-                    {
-                        commonChars.Add(c);
-                    }
-                }
-                workingSet.Clear();
-                workingSet.AddRange(commonChars);
-                commonChars.Clear();
-            }
+        var ans = rucksacks.Chunk(3).Select(elfs => elfs[0].Intersect(elfs[1]).Intersect(elfs[2]).Distinct().First()).Sum(r => r >= 97 ? r - 96 : r - 64 + 26);
 
-            badges.Add(workingSet[0]);
-        }
-
-        var answer = badges.Sum(x =>
-        {
-            if (x >= 97)
-            {
-                return x - 96;
-            }
-            else
-            {
-                return x - 64 + 26;
-            }
-        });
-
-        return new(answer.ToString());
+        return new(ans.ToString());
     }
 }
